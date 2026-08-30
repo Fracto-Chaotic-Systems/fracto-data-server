d4bb2dab29997f66bca82916c11d08c222978622
Mike Hall <mikehallvideo@gmail.com>
2026-08-30 10:58:56 -0400

    Implement service logs endpoint

--- handlers/logs.js
+++ handlers/logs.js
@@ -1,5 +1,5 @@
-// import {process_logfile} from "../../utils/logging.js";
+import {process_logfile} from '../../../utils/logging.js'
 
 export const handle_logs = (req, res) => {
-   // process_logfile('data', res)
+   process_logfile('fracto-data-server', res)
 }
